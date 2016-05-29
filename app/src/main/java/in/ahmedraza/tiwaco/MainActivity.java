74bc43389cbe03c099b758e48c164ef43750e804
ahmedraza <raza2393@gmail.com>
2016-05-29 21:20:11 +0530

    Imageview, EditText and button added in the layout also Button on click activated

--- app/src/main/java/in/ahmedraza/tiwaco/MainActivity.java
+++ app/src/main/java/in/ahmedraza/tiwaco/MainActivity.java
@@ -1,9 +1,17 @@
 package in.ahmedraza.tiwaco;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickButton(View view){
+
+        Log.i("MainActivity", "You Tapped the button");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
