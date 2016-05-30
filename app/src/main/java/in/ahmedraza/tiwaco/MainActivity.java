fa80697f419634471a18a98753623c31dd92281f
ahmedraza <raza2393@gmail.com>
2016-05-30 13:51:57 +0530

    On Click text display feature added

--- app/src/main/java/in/ahmedraza/tiwaco/MainActivity.java
+++ app/src/main/java/in/ahmedraza/tiwaco/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,25 +15,22 @@ public class MainActivity extends AppCompatActivity {
     public void clickButton(View view){
 
         EditText enteredText = (EditText) findViewById(R.id.editText);
-
-
+        TextView updatedText = (TextView) findViewById(R.id.message);
 
         if (enteredText.getText().toString().trim().length()>0){
 
             //int a = Integer.parseInt(enteredText.getText().toString());
             //int multiply = a*30;
-            Toast.makeText(MainActivity.this,"You have entered " + enteredText.getText().toString(), Toast.LENGTH_SHORT ).show();
+            updatedText.setText("You have entered !" +enteredText.getText().toString());
+            updatedText.setPadding(0,20,0,20 );
+
+           // Toast.makeText(MainActivity.this,"You have entered " + enteredText.getText().toString(), Toast.LENGTH_SHORT ).show();
 
         }
         else {
             Toast.makeText(MainActivity.this,"Please Enter number of hours", Toast.LENGTH_SHORT ).show();
 
         }
-
-
-
-
-
 
 
     }
